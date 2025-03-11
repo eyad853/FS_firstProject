8cfc9114256731da141a59ccdcfcc3ba7ec60899
eyad <eyadmosa853@gmail.com>
2025-03-11 05:17:08 +0300

    push

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,18 @@ const connect = async ()=>{
 app.use(cors({
     origin:"https://fs-first-projectfrontend.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true,
 }))
+
+// Extra middleware to confirm headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://fs-first-projectfrontend.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use(express.json())
 
